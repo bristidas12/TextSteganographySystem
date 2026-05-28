7e42a29974c450bbd408d36af8938a54b2ad04ad
bristidas12 <bristidas.ab@gmail.com>
2026-05-28 17:30:32 +0530

    Create src/Steganography/Main.java

---
+++ src/Steganography/Main.java
@@ -0,0 +1,117 @@
+package project;
+
+import java.util.Scanner;
+import java.io.*;
+
+// Main Class
+public class TextSteganographySystem {
+  // Encode Message Method
+    public static void encodeMessage(String coverText,String secretMessage) {
+
+        try {
+
+            FileWriter fw = new FileWriter("encoded.txt");
+
+            fw.write(coverText);
+
+            fw.write("\n\n[HIDDEN MESSAGE]: ");
+
+            fw.write(secretMessage);
+
+            fw.close();
+
+            System.out.println("Message encoded successfully!");
+
+        } catch (IOException e) {
+
+            System.out.println("Error writing file.");
+        }
+    }
+
+    // Decode Message Method
+    public static void decodeMessage() {
+
+        try {
+
+            BufferedReader br =
+                    new BufferedReader(
+                            new FileReader("encoded.txt"));
+
+            String line;
+
+            System.out.println("\n===== FILE CONTENT =====");
+
+            while ((line = br.readLine()) != null) {
+
+                System.out.println(line);
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Error reading file.");
+        }
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int choice;
+
+        do {
+
+            System.out.println("\n===== TEXT STEGANOGRAPHY SYSTEM =====");
+
+            System.out.println("1. Encode Message");
+
+            System.out.println("2. Decode Message");
+
+            System.out.println("3. Exit");
+
+            System.out.print("Enter Your Choice: ");
+
+            choice = sc.nextInt();
+
+            sc.nextLine();
+
+            switch (choice) {
+
+                case 1:
+
+                    System.out.print("Enter Cover Text: ");
+
+                    String coverText = sc.nextLine();
+
+                    System.out.print("Enter Secret Message: ");
+
+                    String secretMessage = sc.nextLine();
+
+                    encodeMessage( coverText,secretMessage );
+
+                    break;
+
+                case 2:
+
+                    decodeMessage();
+
+                    break;
+
+                case 3:
+
+                    System.out.println("Exiting Program...");
+
+                    break;
+
+                default:
+
+                    System.out.println("Invalid Choice.");
+            }
+
+        } while (choice != 3);
+
+        sc.close();
+    }
+}
